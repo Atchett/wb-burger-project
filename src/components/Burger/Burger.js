de1678d5e1639e7e9df5183f1f7e9c96a53d5af6
Atchett <Atchett@users.noreply.github.com>
2020-03-19 21:05:29 +0000

    generic output of ingredients based on state

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,30 @@
+import React from "react";
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
+
+const burger = props => {
+  // Object.keys transforms into an array of keys
+  const transformedIngredients = Object.keys(props.ingredients)
+    // map executes a function on each element in the array
+    .map(igKey => {
+      // creating an array with the number of each item
+      return (
+        [...Array(props.ingredients[igKey])]
+          // creates the component instance
+          .map((_, i) => {
+            // ignore the value but using the index to generate a key
+            return <BurgerIngredient key={igKey + i} type={igKey} />;
+          })
+      );
+    });
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
